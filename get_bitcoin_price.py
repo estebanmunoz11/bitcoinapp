e6fe7b35219d8acaf81973541f99ef09ac9003f7
estebanmunoz11 <84551252+estebanmunoz11@users.noreply.github.com>
2023-12-17 16:08:35 -0500

    Add files via upload

---
+++ get_bitcoin_price.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import datetime
+import os
+
+def get_bitcoin_price():
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {"ids": "bitcoin", "vs_currencies": "usd"}
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        price = data.get("bitcoin", {}).get("usd")
+        if price:
+            result = f"El precio actual de Bitcoin es ${price}"
+        else:
+            result = "No se pudo obtener el precio de Bitcoin."
+    else:
+        result = "Error al conectar con la API de CoinGecko."
+    
+
+    return result
+
+if __name__ == "__main__":
+    result = get_bitcoin_price()
+    print(result)
