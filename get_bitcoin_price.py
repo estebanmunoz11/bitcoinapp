929495fb52b910c167b7dd44a9550220c3dcebf4
estebanmunoz11 <84551252+estebanmunoz11@users.noreply.github.com>
2023-12-21 20:52:53 -0500

    Update get_bitcoin_price.py

--- get_bitcoin_price.py
+++ get_bitcoin_price.py
@@ -1,22 +1,19 @@
 import requests
-from datetime import datetime
-import os
 
 def get_bitcoin_price():
     url = "https://api.coingecko.com/api/v3/simple/price"
     params = {"ids": "bitcoin", "vs_currencies": "usd"}
     response = requests.get(url, params=params)
-    
+
     if response.status_code == 200:
         data = response.json()
         price = data.get("bitcoin", {}).get("usd")
         if price:
-            result = f"El precio actual de Bitcoin es ${price}"
+            result = "El precio actual de Bitcoin es ${}".format(price)  # Usando format()
         else:
             result = "No se pudo obtener el precio de Bitcoin."
     else:
         result = "Error al conectar con la API de CoinGecko."
-    
 
     return result
 
